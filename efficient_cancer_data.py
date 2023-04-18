827f4d8aa38c3b8c4907bd3fabc03629576dda6e
zanderly <abjeldanes@gmail.com>
2023-04-18 13:06:54 -0700

    Update efficient_cancer_data.py

--- efficient_cancer_data.py
+++ efficient_cancer_data.py
@@ -1,7 +1,9 @@
 # Copyright 2013 Philip N. Klein
-from vec import Vec
+# from vec import Vec
 # from vecutil import vec2list
 from sympy import Matrix
+
+import numpy as np
 
 def read_training_data(fname, D=None):
     """Given a file in appropriate format, and given a set D of features,
@@ -29,7 +31,9 @@ def read_training_data(fname, D=None):
         row = line.split(",")
         patient_ID = int(row[0])
         b.append(-1) if row[1] == 'B' else b.append(1)
-        feature_vectors[patient_ID] = Vec(D, {f:float(row[feature_map[f]+2]) for f in D})
+        # feature_vectors[patient_ID] = Vec(D, {f:float(row[feature_map[f]+2]) for f in D})
+        feature_vectors[patient_ID] = np.array(D, {f:float(row[feature_map[f]+2]) for f in D})
+
         A.append(list(feature_vectors[patient_ID]))
         # A.append(vec2list(feature_vectors[patient_ID]))
     return Matrix(A), Matrix(b)
