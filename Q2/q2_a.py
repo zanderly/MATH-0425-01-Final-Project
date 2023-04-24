17f4f437408df3a4ea84ade92e0b07da97e04506
zanderly <abjeldanes@gmail.com>
2023-04-24 14:49:55 -0700

    11

--- Q2/q2_a.py
+++ Q2/q2_a.py
@@ -23,3 +23,34 @@ def least_squares(filename):
     return x
 
 print(least_squares(input_file_name))
+
+filename = "validate.data"
+
+
+def malignant_or_benign(x):
+    # Step 1: Read the training data
+    try:
+        A, b = read_training_data(filename)
+    except FileNotFoundError:
+        raise IOError(f"Error: Could not find file {filename}")
+    except ValueError:
+        raise ValueError(f"Error: Invalid data in file {filename}")
+
+    x = least_squares("input_file_name")
+    # Step 2: Compute the predictions
+
+    predictions = np.dot(A, x)
+
+    # Step 3: Define the classifier function
+
+    def C(y):
+        if y >= 0:
+            return 1
+        else:
+            return -1
+
+
+    # Step 4: Apply the classifier function to the predictions to get the final predictions
+
+
+    # Print the final predictions
