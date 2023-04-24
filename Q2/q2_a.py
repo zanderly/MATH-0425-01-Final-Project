25e008a8d3635ab88aceb7383bfa47caeb9392a1
zanderly <abjeldanes@gmail.com>
2023-04-24 14:07:28 -0700

    3

--- Q2/q2_a.py
+++ Q2/q2_a.py
@@ -4,22 +4,22 @@ from efficient_cancer_data import read_training_data
 input_file_name = "train.data"
 output_file_name = "least_squares_solution.txt"
 
-# Step 1: Read the training data
-try:
-    A, b = read_training_data(input_file_name)
-except FileNotFoundError:
-    raise IOError(f"Error: Could not find file {input_file_name}")
-except ValueError:
-    raise ValueError(f"Error: Invalid data in file {input_file_name}")
+def least_squares(filename):
+    # Step 1: Read the training data
+    try:
+        A, b = read_training_data(filename)
+    except FileNotFoundError:
+        raise IOError(f"Error: Could not find file {filename}")
+    except ValueError:
+        raise ValueError(f"Error: Invalid data in file {filename}")
 
-# Step 2: Compute the QR decomposition of A
-Q, R = np.linalg.qr(A)
+    # Step 2: Compute the QR decomposition of A
+    Q, R = np.linalg.qr(A)
 
-# Step 3: Compute the least squares solution x
-x = np.dot(np.dot(np.linalg.inv(R), Q.T), b)
+    # Step 3: Compute the least squares solution x
+    x = np.dot(np.dot(np.linalg.inv(R), Q.T), b)
 
-# Step 4: Print the solution x
-with open(output_file_name, "w") as f:
-    f.write("The least squares solution is:\n")
-    for i in range(len(x)):
-        f.write(str(x[i][0]) + "\n")
+    # Step 4: Return the solution x
+    return x
+
+print(least_squares(input_file_name))
