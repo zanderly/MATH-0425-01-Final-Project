890c460f95bcc28963a694165a0885bfb5b2445c
zanderly <abjeldanes@gmail.com>
2023-04-23 21:21:21 -0700

    2

--- Q2/q2_a.py
+++ Q2/q2_a.py
@@ -3,19 +3,19 @@ from efficient_cancer_data import read_training_data
 
 filename = "train.data"
 
-
 # Step 1: Read the training data
 try:
     A, b = read_training_data(filename)
 except FileNotFoundError:
-    print(f"Error: Could not find file {filename}")
-    exit(1)
+    raise IOError(f"Error: Could not find file {filename}")
 except ValueError:
-    print(f"Error: Invalid data in file {filename}")
-    exit(1)
+    raise ValueError(f"Error: Invalid data in file {filename}")
 
 # Step 2: Compute the QR decomposition of A
+Q, R = np.linalg.qr(A)
 
 # Step 3: Compute the least squares solution x
+x = np.dot(np.dot(np.linalg.inv(R), Q.T), b)
 
 # Step 4: Print the solution x
+print("The least squares solution is:\n", x)
