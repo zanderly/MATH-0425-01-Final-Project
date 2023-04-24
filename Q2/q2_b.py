e6454b87c2066d7df419e5d3fcf8e077e50ddfd6
zanderly <abjeldanes@gmail.com>
2023-04-24 14:12:56 -0700

    32

--- Q2/q2_b.py
+++ Q2/q2_b.py
@@ -1,33 +1,36 @@
 import numpy as np
 
 from efficient_cancer_data import read_training_data
+from q2_a import least_squares
 
 filename = "validate.data"
 
-# Step 1: Read the training data
-try:
-    A, b = read_training_data(filename)
-except FileNotFoundError:
-    raise IOError(f"Error: Could not find file {filename}")
-except ValueError:
-    raise ValueError(f"Error: Invalid data in file {filename}")
+
+def malignant_or_benign(x):
+    # Step 1: Read the training data
+    try:
+        A, b = read_training_data(filename)
+    except FileNotFoundError:
+        raise IOError(f"Error: Could not find file {filename}")
+    except ValueError:
+        raise ValueError(f"Error: Invalid data in file {filename}")
+
+    x = least_squares("train.data")
+    # Step 2: Compute the predictions
+
+    predictions = np.dot(A, x)
+
+    # Step 3: Define the classifier function
+
+    def C(y):
+        if y >= 0:
+            return 1
+        else:
+            return -1
 
 
-# Step 2: Compute the predictions
-
-predictions = np.dot(A, x)
-
-# Step 3: Define the classifier function
-
-def C(y):
-    if y >= 0:
-        return 1
-    else:
-        return -1
+    # Step 4: Apply the classifier function to the predictions to get the final predictions
 
 
-# Step 4: Apply the classifier function to the predictions to get the final predictions
-
-
-# Print the final predictions
+    # Print the final predictions
 
