ccbc2d547ffb8a00942fe2aed48046cb4d901236
zanderly <abjeldanes@gmail.com>
2023-04-27 13:08:39 -0700

    3232

--- Q1/PageRank.py
+++ Q1/PageRank.py
@@ -4,27 +4,14 @@ input_file_name = "top250movies.txt"
 output_file_name = "ranked_actors.txt"
 
 # Read the data from the file and create the network
-try:
-    G = nx.Graph()
-    with open(input_file_name, encoding="utf-8") as f:
-        for line in f:
-            cast = line.strip().split("/")
-            for i in range(1, len(cast)):
-                for j in range(1, i):
-                    actor1 = cast[i]
-                    actor2 = cast[j]
-                    if G.has_edge(actor1, actor2):
-                        G[actor1][actor2]["weight"] += 1
-                    else:
-                        G.add_edge(actor1, actor2, weight=1)
-
-    # Compute the PageRank vector and write the list of ranked actors to a file
-    pr = nx.pagerank(G)
-    ranked_actors = sorted(pr, key=pr.get, reverse=True)
-    with open(output_file_name, "w", encoding="utf-8") as out_file:
-        for actor in ranked_actors:
-            out_file.write(actor + "\n")
-except FileNotFoundError:
-    print("Error: " + input_file_name + " not found.")
-except Exception as e:
-    print("An error occurred:", e)
+# try:
+#     G = nx.Graph()
+#     with open(input_file_name, encoding="utf-8") as f:
+        
+#     with open(output_file_name, "w", encoding="utf-8") as out_file:
+#         for actor in ranked_actors:
+#             out_file.write(actor + "\n")
+# except FileNotFoundError:
+#     print("Error: " + input_file_name + " not found.")
+# except Exception as e:
+#     print("An error occurred:", e)
