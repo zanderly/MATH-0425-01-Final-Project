5ba9266ebc4064bb3a59cdf20f9dd1c5bfae1143
zanderly <abjeldanes@gmail.com>
2023-05-09 17:01:56 -0700

    432342

--- Q1/web_stanford_pagerank.py
+++ Q1/web_stanford_pagerank.py
@@ -11,35 +11,47 @@ from numpy import linalg as LA
 
 from PageRank_module import pageRank
 
+file = "web_stanford.txt"
 # PageRank for mini-web links for Stanford
 links=set()
-with open("q1/web_stanford.txt","r") as f:
-    for line in f:
-        
-        #print(line)
-        ls=line.rstrip("\n").split("/")
-        #print(set(map(int, ls)))
-        links.update(set(map(int, ls)))
+try: 
+    with open(file,"r") as f:
+        for line in f:
+            
+            #print(line)
+            ls=line.rstrip("\n").split("/")
+            #print(set(map(int, ls)))
+            links.update(set(map(int, ls)))
+except FileNotFoundError:
+    raise IOError(f"Error: Could not find file {file}.")
+except ValueError:
+    raise ValueError(f"Error: Invalid data in file {file}.")
+  
 
-f.close()        
+print(links)
+
 # Create adjacency matrix
 links=sorted(links)  #sort the IDs of the webpages into a list
 n = len(links)
 G = zeros((n,n))
 
-with open("q1/web_stanford.txt","r") as f:
-    for line in f:
-        ls=line.rstrip("\n").split("/")
-        lIDs = map(int, ls) #IDs on the line
-        row = [] # row indices of IDS
-        for i in lIDs:
-            row.append(links.index(i))
-            
-        j = row[0] # First element is column index
-        # Link page j to page i, i.e. set G_{ij} to 1 
-        G[row[1:],j] = 1.0
-
-f.close()  
+try:   
+    with open(file,"r") as f:
+        for line in f:
+            ls=line.rstrip("\n").split("/")
+            lIDs = map(int, ls) #IDs on the line
+            row = [] # row indices of IDS
+            for i in lIDs:
+                row.append(links.index(i))
+                
+            j = row[0] # First element is column index
+            # Link page j to page i, i.e. set G_{ij} to 1 
+            G[row[1:],j] = 1.0
+except FileNotFoundError:
+    raise IOError(f"Error: Could not find file {file}.")
+except ValueError:
+    raise ValueError(f"Error: Invalid data in file {file}.")
+print("The adjacency matrix is: \n", G)
 
 netw=pageRank(G,links,0.85,1e-6,15)  
 prv = netw.linsolve()
